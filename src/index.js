4b8800a7cbaa2075e8b943eb333aaed6d117ec6e
boneswvb@gmail.com <boneswvb@gmail.com>
2019-03-25 04:08:50 +0200

    update with current changes. Add menu items still not working

--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import MainMenu from './js/MainMenu';
 import * as serviceWorker from './serviceWorker';
+import DropDownMenuItems from './js/DropDownMenuItems';
 
-ReactDOM.render(<MainMenu />, document.getElementById('root'));
+ReactDOM.render(<MainMenu DropDownMenuItems={DropDownMenuItems} />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- src/js/DropDownMenuItems.js
+++ src/js/DropDownMenuItems.js
@@ -1,52 +1,52 @@
-export const DropDownMenuItems = [
+ export const DropDownMenuItems = [
   {
     id: 1,
     name: 'Tribute to Gen. De La Rey',
-    href: 'href="#pj1"',
+    href: '"#pj1"',
   },
   {
     id: 2,
     name: 'Radio Vision',
-    href: 'href="#pj2"',
-  }
+    href: '"#pj2"',
+  },
    {
     id: 3,
     name: 'Quote generator',
-    href: 'href="#pj3"',
+    href: '"#pj3"',
   },
    {
     id: 4,
     name: 'Show Local Weather',
-    href: 'href="#pj4"',
+    href: '"#pj4"',
   },
    {
     id: 5,
     name: 'Wikipedia Viewer',
-    href: 'href="#pj5"',
+    href: '"#pj5"',
   },
    {
     id: 6,
     name: 'Twitch Streamer',
-    href: 'href="#pj6"',
+    href: '"#pj6"',
   },
    {
     id: 7,
     name: 'Calulator',
-    href: 'href="#pj7"',
+    href: '"#pj7"',
   },
    {
     id: 8,
     name: 'Pomodoro Clock',
-    href: 'href="#pj8"',
+    href: '"#pj8"',
   },
    {
     id: 9,
     name: 'Tic Tac Toe Game',
-    href: 'href="#pj9"',
+    href: '"#pj9"',
   },
    {
     id: 10,
     name: 'Simon Game',
-    href: 'href="#pj10"',
+    href: '"#pj10"',
   }
-};
+];
